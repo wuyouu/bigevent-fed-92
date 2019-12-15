eba10fa6d777151f6930e4f648979864a4d04e6e
wuyouu <995347291@qq.com>
2019-12-15 16:49:30 +0800

    修改示例

--- web_back/tools/config.js
+++ web_back/tools/config.js
@@ -9,6 +9,6 @@
 
 var BASE_URL = 'http://localhost:8000'; // 通过变量保存基地址
 
-// 后续的接口地址都基于基地址拼接即可
+//  后续的接口地址都基于基地址拼接即可
 var LOGIN = BASE_URL + '/admin/login';
 var LOGOUT = BASE_URL + '/admin/logout';
